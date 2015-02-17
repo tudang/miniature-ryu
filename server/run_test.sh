1d9801aa5c896f64c111be4b5483a7afe27e2b5e
Tu Dang <danghu@usi.ch>
2015-02-17 11:53:27 +0100

    simplify filename format

--- server/run_test.sh
+++ server/run_test.sh
@@ -9,25 +9,24 @@ ps="$1"
 EXEC_DIR="/home/danghu/miniature-ryu"
 DATA="/home/danghu/miniature-ryu/server/data"
 LOG="/home/danghu/miniature-ryu/server/log"
-servers=( "node86" "node88" "node85" )
-#clients=( "node81" "node82" "node83" )
-clients=( "node81" )
+servers=( "node86" "node88" )
+clients=( "node81" "node82" )
 
 
 function server { 
 echo "start server $1"
-ssh $1 "nohup $EXEC_DIR/server/async_server.py $DATA/$1-$2.txt \
-               >$LOG/$1-$2.log 2>&1 &"
+ssh $1 "nohup $EXEC_DIR/server/async_server.py $DATA/$1.txt \
+               >$LOG/$1.log 2>&1 &"
 }
 
 function client {
 echo "run client $1"
 ssh $1 "nohup $EXEC_DIR/client/async_client.py $2 host86 $ps \
-                $DATA/$1-$2.txt >$LOG/$1-$2.log 2>&1 &"
+                $DATA/$1.txt >$LOG/$1.log 2>&1 &"
 }
 
 #clean data and log
-#rm $LOG/* $DATA/*
+rm $LOG/* $DATA/*
 
 for i in "${servers[@]}"
 do
@@ -41,7 +40,7 @@ do
   cid=$[cid+1]
 done
 
-sleep 10
+sleep 15
 echo "Terminate servers"
 
 for i in "${servers[@]}"
@@ -51,11 +50,11 @@ do
 done
 
 echo "Hamming Distance"
-set -- "node86" "node88" "node85"
+set -- "node86" "node88" 
 for a; do 
   shift
   for b; do
-    d=`diff -u $DATA/$a-$ps.txt $DATA/$b-$ps.txt | grep ^[+-].[0-9] | wc -l`
+    d=`diff -u $DATA/$a.txt $DATA/$b.txt | grep ^[+-].[0-9] | wc -l`
     printf "%s - %s: %d\n" "$a" "$b" $d
   done
 done
@@ -64,3 +63,6 @@ done
 #d=`diff -u $DATA/node70-$ps.txt $DATA/node88-$ps.txt | grep ^[+-].[0-9] | wc -l`
 #echo $d
 echo "Experiment ended."
+
+wc data/* 
+tail log/*
