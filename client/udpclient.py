0e04f45ac2e2b5c9e79e9a0b8661a5de4a6e2ad0
Tu Dang <danghu@usi.ch>
2015-02-22 12:30:50 +0100

    add client options

--- client/udpclient.py
+++ client/udpclient.py
@@ -1,39 +1,53 @@
 #!/usr/bin/python
 '''
     udp socket client
-    Silver Moon
 '''
 
  
-import socket   #for sockets
-import sys  #for exit
+import socket   
+import sys, optparse 
  
-# create dgram udp socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
- 
-host = sys.argv[1]
-port = 8888;
-x = int(sys.argv[2])
-N = x * 2**10
-cid='1'
-for i in range(1,N):
-    msg = str(i) + "," + cid
-     
-    try :
-        #Set the whole string
-        s.sendto(msg, (host, port))
-         
-        # receive data from client (data, addr)
-        #d = s.recvfrom(1024)
-        #reply = d[0]
-        #addr = d[1]
-         
-        #print 'Server reply : ' + reply
-     
-    except socket.error, msg:
-        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+def parse_args():
+    usage = '''usage: %prog [options] server-address'''
+    
+    parser = optparse.OptionParser(usage)
+  
+    help = "The sending throughput"
+    parser.add_option('--bw', help=help, default=1)
+
+    help = "Client ID"
+    parser.add_option('--id', help=help, default='1')
+  
+    options, args = parser.parse_args()
+  
+    if len(args) != 1:
+        parser.error('Provide server address')
+  
+    s_addr = args[0]
+  
+    return options, s_addr
+
+def main():
+    options, host = parse_args()
+    # create dgram udp socket
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error:
+        print 'Failed to create socket'
         sys.exit()
+     
+    port = 8888;
+    N = options.bw * 2**10
+    cid = options.id
+    for i in range(1,N):
+        msg = str(i) + "," + cid
+         
+        try :
+            sent = s.sendto(msg, (host, port))
+            print sent 
+        except socket.error, msg:
+            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
+
+if __name__ == '__main__':
+    main()
