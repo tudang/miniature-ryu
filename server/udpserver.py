04cc7ad8b158237547558cc81d5c3ae4903266a2
Tu Dang <danghu@usi.ch>
2015-02-26 14:02:37 +0100

    catch Error when computing throughput

--- server/udpserver.py
+++ server/udpserver.py
@@ -4,24 +4,33 @@
     Tu Dang (huynh.tu.dang@usi.ch)
 '''
  
-import socket, select, optparse, sys, signal, cStringIO
+import socket, select
+import optparse, sys, signal
+import cStringIO, string
+import logging
+
 from datetime import datetime
 
 class UDPServer(object):
 
     def break_handler(self, signal, frame):
-        print "Terminate server"
-        with open(self.output, 'w+') as f:
-            f.write(self.oc.getvalue())
-        print "write to file %s" % self.output
-        td = self.end - self.start
-        bw = self.total * 8 / total_seconds(td) / 2**20
-        print 'received: ', self.total
-        print 'total seconds %f' % total_seconds(td)
-        print 'bandwidth %3.2f Mbps' % bw
-        self.sock.close()
-        self.oc.close()
-        sys.exit()
+        try:
+            print "Terminate server"
+            with open(self.output, 'w+') as f:
+                f.write(self.oc.getvalue())
+            print "write to file %s" % self.output
+            td = self.end - self.start
+            bw = self.total * 8 / total_seconds(td) / 2**20
+            print 'received: ', self.total
+            print 'total seconds %f' % total_seconds(td)
+            print 'bandwidth %3.2f Mbps' % bw
+        except ZeroDivisionError:
+            print "Do not received anything"
+        finally:
+            print "Cleanup"
+            self.sock.close()
+            self.oc.close()
+            sys.exit()
 
 
     def close(self):
@@ -34,10 +43,29 @@ class UDPServer(object):
         print 'received: ', self.total
         print 'total seconds %f' % total_seconds(td)
         print 'bandwidth %3.2f Mbps' % bw
+        lost = self.count_lost()
+        print 'lost %f' % lost   
+        with open("record.txt", 'a+') as lf:
+            lf.write(self.iface + ',' + str(bw) + ',' + str(lost) + '\n')
         self.sock.close()
         self.oc.close()
         sys.exit()
 
+    def count_lost(self):
+        current = 1
+        lost = 0
+        lines = string.split(self.oc.getvalue(), '\n')
+        for line in lines:
+            try:
+                seq = int(line[:8])
+                if seq != current:
+                    #print seq, current
+                    lost += seq - current
+                current = seq + 1
+            except ValueError:
+                current += 1
+        print lost
+        return float(lost) / current
 
     def __init__(self, iface, port,  output):
         self.iface = iface
@@ -55,7 +83,7 @@ class UDPServer(object):
         try :
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, self.iface + "\0")
+            #s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, self.iface + "\0")
             print 'Socket created'
         except socket.error, msg :
             print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
@@ -81,17 +109,17 @@ class UDPServer(object):
         #print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()[:10]
 
 def parse_args():
-    usage = """usage: %prog [options]
+    usage = """
+            usage: %prog [options]
+            run it like this:
+            sudo python udpserver.py eth1
+            """
 
-run it like this:
-sudo python udpserver.py eth1
-
-"""
     parser = optparse.OptionParser(usage)
     help = "The port to listen on. Default to 8888."
     parser.add_option('--port', type='int', help=help, default=8888)
 
-    help = "buffer size"
+    help = "buffer size. Default 4kB"
     parser.add_option('--bufsize', type='int', help=help, default=4096)
 
     help = "The output filename. Default is output.txt"
@@ -118,7 +146,7 @@ def main():
     udpserver.oc.write(data.strip()[:10] + '\n')
     print "Received from %s" % addr[0]
     udpserver.sock.setblocking(False)
-    ready = select.select([udpserver.sock], [], [], 5)
+    ready = select.select([udpserver.sock], [], [], 2)
     while ready[0]:
         udpserver.read_socket(BUFSIZE)
         ready = select.select([udpserver.sock], [], [], 5)
