0df311ad2a8c81ca233d73b9edd95757412cda15
Tu Dang <danghu@usi.ch>
2015-05-05 23:15:59 +0200

    c client

--- client/client.c
+++ client/client.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <time.h>
 
+#define MAX 1470
+
 void error(const char *msg)
 {
     perror(msg);
@@ -22,14 +24,8 @@ int main(int argc, char *argv[])
     unsigned int length;
     struct sockaddr_in server, from;
     struct hostent *hp;
-    char buffer[256];
+    char buffer[MAX];
     struct timespec tstart={0,0}, tend={0,0}, res;
-    int rc; 
-    rc = clock_getres(CLOCK_MONOTONIC, &res);
-    if (!rc)
-        printf("CLOCK_MONOTONIC: %ldns\n", res.tv_nsec);
-    rc = clock_getres(CLOCK_MONOTONIC_COARSE, &res);
-        printf("CLOCK_MONOTONIC_COARSE: %ldns\n", res.tv_nsec);
 
     if (argc != 3) { printf("Usage: server port\n");
                         exit(1);
@@ -48,22 +44,35 @@ int main(int argc, char *argv[])
     length = sizeof(struct sockaddr_in);
     struct timespec req = {0};
     int milisec = 1;
+    int micro = 1;
     req.tv_sec = 0;
-    req.tv_nsec = milisec * 1.0e6;
-    
-    for(i=1; i < 10; i++) {
-        bzero(buffer, 256);
-        sprintf(buffer, "%08d\n", i);
-        clock_gettime(CLOCK_MONOTONIC_COARSE, &tstart);
+    req.tv_nsec = micro * 1.0e3;
+    long total;
+    clock_gettime(CLOCK_REALTIME, &tstart);
+    int npacket=1.0e6;
+    for(i=0; i < npacket; i++) {
+        memset(buffer, '@', 1470);
+        char msgid[8]; 
+        sprintf(msgid, "%08d", i);
+        strncpy(buffer, msgid, 8);
+        clock_gettime(CLOCK_REALTIME, &tstart);
         n = sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&server, length);
-        nanosleep(&req, (struct timespec *)NULL);
-        clock_gettime(CLOCK_MONOTONIC_COARSE, &tend);
-        printf("some_long_computation took about %.5f milliseconds\n",
-        ((double)tend.tv_sec*1.0e3 + 1.0e-6*tend.tv_nsec) - 
-           ((double)tstart.tv_sec*1.0e3 + 1.0e-6*tstart.tv_nsec));
-        printf("%08d\n", i);
         if (n < 0) error("sendto");
+        total += n;
+        //nanosleep(&req, (struct timespec *)NULL);
+        clock_gettime(CLOCK_REALTIME, &tend);
+        double duration = ((double)tend.tv_sec*1E9 + tend.tv_nsec) - 
+        ((double)tstart.tv_sec*1E9 + tstart.tv_nsec);
+        printf("some_long_computation took about %.5f nanoseconds\n", duration);
     }
+    //fwrite(buffer, 16, 1, stdout);
+    clock_gettime(CLOCK_REALTIME, &tend);
+    //printf("strlen(buffer): %d\n", strlen(buffer));
+    double duration = ((double)tend.tv_sec*1.0e3 + 1.0e-6*tend.tv_nsec) - 
+    ((double)tstart.tv_sec*1.0e3 + 1.0e-6*tstart.tv_nsec);
+    printf("some_long_computation took about %.5f milliseconds\n", duration);
+    printf("Total bytes sent %d\n", total);
+    printf("Throughput: %.2f\n", ((double)total * 8 / duration * 1.0e-6));
     //n = recvfrom(sock, buffer, 256, 0, (struct sockaddr *) &from, &length);
     //    //if (n < 0) error("recvfrom");
     //        //write(1, "Got an ack: ",12);
