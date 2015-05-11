e11bde9649843a2019a8f645be7b56c46eddecd2
Tu Dang <danghu@usi.ch>
2015-05-12 01:00:04 +0200

    server find majority at the head of itf queues

--- server/server.c
+++ server/server.c
@@ -5,23 +5,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #define PORT 8888
 #define SIZE 1470
-
+#define MAX_NUM 1000000 
 pthread_t tid[4]; // this is thread identifier
 
+int values[4][MAX_NUM];  // value queues
+bool start = false;
+int counter = 0;
+pthread_mutex_t lock;
+int idx = 0;
+struct thread_args {
+    int index;
+    char *itf;
+};
 /* This is thread function */
 void *recvFunc(void *arg)
 {
-    int sockfd,n;
+    int sockfd, n;
     struct sockaddr_in servaddr,cliaddr;
     socklen_t len;
     char mesg[SIZE];
     char *itf;
-    itf = (char*)arg;
+    itf = (char*)arg; 
+    pthread_mutex_lock(&lock);
+    int index = idx++;
+    pthread_mutex_unlock(&lock);
+    pthread_t self_id;
+    self_id = pthread_self();
+    printf("\nHello from thread %u\n",self_id);
+    printf("index:%d interface %s\n", index, itf);
     char last_msg[9];
     int last_id = 0;
+    int inst = 0;
     sockfd=socket(AF_INET,SOCK_DGRAM,0);
     if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, 
                             itf, strlen(itf)) < 0) 
@@ -38,23 +56,80 @@ void *recvFunc(void *arg)
     
     for (;;)
     {
-       len = sizeof(cliaddr);
-       n = recvfrom(sockfd,mesg,SIZE,0,(struct sockaddr *)&cliaddr,&len);
-       //sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-       printf("[%s]%.8s\n", itf, mesg);
+        len = sizeof(cliaddr);
+        n = recvfrom(sockfd,mesg,SIZE,0,(struct sockaddr *)&cliaddr,&len);
+        //sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+        //printf("[%s]%.8s\n", itf, mesg);
         strncpy(last_msg, mesg, 8);
         last_msg[9] = '\0'; // place the null terminator
         last_id = atoi(last_msg);
+        values[index][inst++] = last_id;  
+        if (counter < inst) counter = inst;
         if (last_id >= 999900) break; 
     }
         pthread_exit(&last_id);
     return NULL;
 }
+
+void *evalFunc(void *args)
+{
+    int learn[MAX_NUM];
+    while (counter == 0) {sleep(1);}
+    
+    int j, i = 0, k = 0;
+    while (i < counter) {
+        int an_instance[4];
+        for (j = 0; j < 4; j++) {
+            if (values[j][i] != 0) {
+                //printf("%.8d  ", values[j][i]);
+                an_instance[j] = values[j][i];
+            } else {
+                an_instance[j] = -1; 
+                //printf("%8s ", "");
+            }
+        }
+        int selected = findMajorityElement(an_instance, 4); 
+        printf("%8d\n", selected);
+        learn[k++] = selected;
+        i++;
+        if (i == counter) sleep(1);
+    }
+    printf("End eval: counter=%d\n", counter);
+}
+
+
+int findMajorityElement(int* arr, int size) {
+    int count = 0, i, majorityElement;
+    for (i = 0; i < size; i++) {
+        if (count == 0)
+            majorityElement = arr[i];
+        if (arr[i] == majorityElement)
+            count++;
+        else
+            count--;
+    }
+    count = 0;
+    for (i = 0; i < size; i++)
+        if (arr[i] == majorityElement)
+            count++;
+    if (count > size/2)
+        return majorityElement;
+    return -1;
+}
+
+
 int main(int argc, char**argv)
 {
     int i, err;
     int count = 0;
     int *ptr[4];
+    
+     if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+
 
     for(i = 1; i < argc; i++) {
         err = pthread_create(&tid[count++], NULL, recvFunc, argv[i]);
@@ -62,8 +137,12 @@ int main(int argc, char**argv)
             perror("Thread create Error");
             exit(1);
         }
-        else 
-            printf("Thread created successfully\n");
+    }
+
+    err = pthread_create(&tid[count++], NULL, evalFunc, (void *)NULL);
+    if (err != 0) {
+        perror("Thread create Error");
+        exit(1);
     }
 
     for(i = 0; i < count; i++) {
