d25123a4b628579b3f17aea63c8b4311e9cda81b
Tu Dang <tudang87@gmail.com>
2015-08-23 12:18:13 +0200

    remove unused files

--- netpaxos/node-conf/check-diff.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-diff /tmp/eth0.capture /tmp/eth1.capture
-diff /tmp/eth0.capture /tmp/eth2.capture
-diff /tmp/eth0.capture /tmp/eth3.capture
--- netpaxos/node-conf/check-order.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ncol=$(awk '{print NF; exit}' $1)
-for i in `seq 1 $ncol`; do
-    eval "awk '{print \$$i}' $1 | ./validate"
-done
