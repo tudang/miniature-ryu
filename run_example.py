9b1cca289bcd0c317a5d2e60919b9eb2d412dbff
Tu Dang <danghu@usi.ch>
2015-09-14 17:48:18 +0200

    update script

--- run_example.py
+++ run_example.py
@@ -5,8 +5,12 @@ import shlex
 import os
 from threading import Timer
 import time
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def replica(host, path, duration, num_instances, eth, output):
+    logger.info("start replica %s" % host)
     cmd = "ssh %s %s/replica -s -t %d -n %d %s" % (host, path, duration, num_instances, eth)
     with open("%s/%s.dat" % (output, host), 'w+') as out: 
         proc = subprocess.Popen(shlex.split(cmd),
@@ -16,13 +20,14 @@ def replica(host, path, duration, num_instances, eth, output):
     return proc
 
 
-def client(path, rep_id, duration, num_instances, eth):
-    cmd = "%s/replica -c -t %d -n %d %s" % (path, duration, num_instances, eth)
+def client(host, path, duration, num_instances, eth):
+    logger.info("start client %s" % host)
+    cmd = "ssh %s %s/replica -c -t %d -n %d %s" % (host, path, duration, num_instances, eth)
     proc = subprocess.Popen(shlex.split(cmd), shell=False)
     return proc
 
 def send_term_signal(host):
-    #logger.info("end %s" % host)
+    logger.debug("end %s" % host)
     cmd = "ssh %s pkill -TERM replica" % host
     proc = subprocess.Popen(shlex.split(cmd),
                         shell=False)
@@ -45,23 +50,26 @@ def main():
 
     path = os.getcwd() + "/build"
     args.eth = "enp1s0"
-    pipes = []
 
-    servers = ["node80", "node87"]
+    servers = ["node84", "node85", "node86", "node87"]
+    clients = ["node81", "node82"]
     # start replicas
     for host in servers:
-        pipes.append(replica(host, path, args.time, args.instance, args.eth, args.output))
+        replica(host, path, args.time, args.instance, args.eth, args.output)
 
-    time.sleep(3)
+    time.sleep(5)
 
     # start client
-    for idx in range(1):
-        pipes.append(client(path, idx, args.time, args.instance, args.eth))
+    for idx in range(10):
+        for cli in clients:
+            client(cli, path, args.time, args.instance, args.eth)
 
     
     # kill replicas and client after (args.time) seconds
-    t= Timer(args.time, kill_all, servers)
+    t= Timer(args.time, kill_all, clients)
     t.start()
+    s= Timer(args.time, kill_all, servers)
+    s.start()
    
 
 if __name__ == "__main__":
